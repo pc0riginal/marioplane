d8a73771d16384e39927795d141d40f70f82160e
Your Name <you@example.com>
2019-02-23 11:25:12 +0530

    Updates

---
+++ precache-manifest.49df006bce816f08f1baf1f6af324439.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05ec015b6ee7e4e3f7a6",
+    "url": "/marioplane/static/js/runtime~main.05ec015b.js"
+  },
+  {
+    "revision": "0299d6791bd44106f729",
+    "url": "/marioplane/static/js/main.0299d679.chunk.js"
+  },
+  {
+    "revision": "df1716bc755759a86820",
+    "url": "/marioplane/static/js/2.df1716bc.chunk.js"
+  },
+  {
+    "revision": "0299d6791bd44106f729",
+    "url": "/marioplane/static/css/main.0121996f.chunk.css"
+  },
+  {
+    "revision": "0bd0438ed6af54fefc83ecc31f6612be",
+    "url": "/marioplane/index.html"
+  }
+];
